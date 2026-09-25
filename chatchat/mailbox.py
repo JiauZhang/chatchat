5b3e6b5ac2209c433a325aba8897ae50ae204d35
JiauZhang <1743960454@qq.com>
2026-09-25 18:39:12 +0800

    chore: delete the unused re-export modules

--- chatchat/mailbox.py
+++
@@ -1,9 +0,0 @@
-from chatchat.core.mailbox import (Mailbox, Message,
-                                   format_teammate_batch,
-                                   idle_notification,
-                                   is_structured_protocol_message,
-                                   task_assignment)
-
-__all__ = ['Mailbox', 'Message', 'format_teammate_batch',
-           'is_structured_protocol_message', 'idle_notification',
-           'task_assignment']
--- chatchat/tools.py
+++
@@ -1,5 +0,0 @@
-from chatchat.core.tools import (create_agent, send_message, task_create,
-                                 task_get, task_list, task_stop, task_update)
-
-__all__ = ['send_message', 'create_agent', 'task_stop', 'task_create',
-           'task_list', 'task_get', 'task_update']
